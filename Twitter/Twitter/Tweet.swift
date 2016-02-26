7dd5ebdcf09a786b73272087bb02c16e18766e5f
mariella725 <mariella.725@icloud.com>
2016-02-25 19:47:17 -0500

    Added files via upload

---
+++ Twitter/Twitter/Tweet.swift
@@ -0,0 +1,69 @@
+//
+//  Tweet.swift
+//  twitter
+//
+//  Created by Mariella Sypa on 2/14/16.
+//  Copyright © 2016 Mariella Sypa. All rights reserved.
+//
+
+import UIKit
+
+class Tweet: NSObject {
+    var user: User?
+    var text: String?
+    var createdAtString: String?
+    var createdAt: NSDate?
+    var timePassed: Int?
+    var timeSince: String!
+    var favoriteCount: Int?
+    var retweetCount: Int?
+    
+    init(dictionary: NSDictionary){
+        user = User(dictionary: dictionary["user"] as! NSDictionary)
+        text = dictionary["text"] as? String
+        createdAtString = dictionary["created_at"] as? String
+        favoriteCount = dictionary["favorite_count"] as? Int
+        retweetCount = dictionary["retweet_count"] as? Int
+        
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "EEE MMM d HH:mm:ss Z y"
+        createdAt = formatter.dateFromString(createdAtString!)
+        
+        let now = NSDate()
+        let then = createdAt 
+        timePassed = Int(now.timeIntervalSinceDate(then!))
+
+        //function from someone on slack
+        if timePassed >= 86400 {
+            timeSince = String(timePassed! / 86400)+"d"
+        }
+        if (3600..<86400).contains(timePassed!) {
+            timeSince = String(timePassed!/3600)+"h"
+        }
+        if (60..<3600).contains(timePassed!) {
+            timeSince = String(timePassed!/60)+"m"
+        }
+        if timePassed < 60 {
+            timeSince = String(timePassed!)+"s"
+        }
+    }
+    
+
+    class func tweetsWithArray(array: [NSDictionary]) -> [Tweet] {
+        var tweets = [Tweet]()
+        
+        for dictionary in array {
+            tweets.append(Tweet(dictionary: dictionary))
+        }
+        
+        return tweets
+    }
+    
+    class func tweetAsDictionary(dict: NSDictionary) -> Tweet {
+        
+        let tweet = Tweet(dictionary: dict)
+        
+        return tweet
+    }
+    
+}
---
+++ Twitter/Twitter/tweetsViewController.swift
@@ -0,0 +1,68 @@
+//
+//  TweetsViewController.swift
+//  Twitter
+//
+//  Created by Mariella Sypa on 2/15/16.
+//  Copyright © 2016 Mariella Sypa. All rights reserved.
+//
+
+import UIKit
+
+class tweetsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    var tweets: [Tweet]?
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        TwitterClient.sharedInstance.homeTimeLineWithParams(nil, completion: { (tweets, error) -> () in
+            self.tweets = tweets
+            self.tableView.reloadData()
+        })
+        
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tweets != nil {
+            return tweets!.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("tweetCell", forIndexPath: indexPath) as! TweetCell
+        
+        cell.selectionStyle = .None
+        cell.tweet = tweets![indexPath.row]
+        
+        return cell
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func onLogout(sender: AnyObject) {
+        User.currentUser?.logout()
+    }
+    
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPathForCell(cell)
+        let tweetViewController = segue.destinationViewController as! singleTweetViewController
+        
+        tweetViewController.tweets = tweets![(indexPath?.row)!]
+        
+        print("prepare for segue called!!")
+    }
+    
+}
